e8bdb093f3c8179ff381128d8f6bcac960d53d29
mitten-cell <73669155+mitten-cell@users.noreply.github.com>
2023-01-13 13:31:37 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-c35abv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-c35abv)
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+calculations my personel onsecuritypolicyviolation
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
